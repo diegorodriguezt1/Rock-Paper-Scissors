0f2051efb071a3df512ca0de871c5d05e095dd73
Diego Rodriguez <diegorodrigueztandil1@gmail.com>
2022-08-03 16:54:56 -0300

    Now we added all the options for the matches with 'if' and 'else if'. Each one with a 'console.log' telling the winner of the match.

--- scripts.js
+++ scripts.js
@@ -12,3 +12,28 @@ function playRound(playerSelection, computerChoice) {
         playerSelection = prompt("What you gonna choose?", "")
         playerSelection = playerSelection.toUpperCase()
     }
+    if (playerSelection === "ROCK" && computerChoice === "SCISSORS") {
+        console.log("You Win! Rock beats Scissors")
+    }
+    else if (playerSelection === "PAPER" && computerChoice === "SCISSORS") {
+        console.log("You Lose! Scissors cut Paper")
+    }
+    else if (playerSelection === "ROCK" && computerChoice === "PAPER") {
+        console.log("You Lose! Paper wrap Rock")
+    }
+    else if (playerSelection === "PAPER" && computerChoice === "ROCK") {
+        console.log("You Win! Paper wrap Rock")
+    }
+    else if (playerSelection === "SCISSORS" && computerChoice === "ROCK") {
+        console.log("You Lose! Rock beats Scissors")
+    }
+    else if (playerSelection === "SCISSORS" && computerChoice === "PAPER") {
+        console.log("You Win! Scissors cut Paper")
+    }
+    else if (playerSelection === computerChoice) {
+        console.log("It's a Tie!!!")
+    }
+}
+
+computerChoice = getComputerChoice();
+console.log(playRound(playerSelection, computerChoice));
