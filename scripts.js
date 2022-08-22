263c943d10c96a35656cbbd01c6f299c9a730f79
Diego Rodriguez <diegorodrigueztandil1@gmail.com>
2022-08-22 16:30:06 -0300

    Change the game function, so when the user or the pc comes to 5 points, activate the finish function to end the game. Added all the constants that target the divs, and show the info in the page. Also the events listener were created in each botton to choose one of the three options to play.

--- scripts.js
+++ scripts.js
@@ -6,57 +6,84 @@ function getComputerChoice() {
     return (pcDecision);
 }
 
-let computerChoice = getComputerChoice();
 let playerSelection = "";
 let userPoints = 0;
 let pcPoints = 0;
+let relator = "";
+let match = "";
 
-function playRound(playerSelection, computerChoice) {
-    while (!(playerSelection === "ROCK" ||playerSelection === "PAPER" ||playerSelection === "SCISSORS")) {
-        playerSelection = prompt("What you gonna choose?", "")
-        playerSelection = playerSelection.toUpperCase()
-    }
+const body = document.body
+const rock = document.querySelector(".rock")
+const paper = document.querySelector(".paper")
+const scissors = document.querySelector(".scissors")
+const round = document.querySelector(".round")
+const userScore = document.querySelector(".user")
+const pcScore = document.querySelector(".pc")
+const finish = document.querySelectorAll(".active")
+
+function playRound(playerSelection) {
+    let computerChoice = getComputerChoice();
     if (playerSelection === "ROCK" && computerChoice === "SCISSORS") {
         ++userPoints;
-        return ("You Win! Rock beats Scissors");
+        return relator = "You Win! Rock beats Scissors"
     }
     else if (playerSelection === "PAPER" && computerChoice === "SCISSORS") {
         ++pcPoints;
-        return ("You Lose! Scissors cut Paper")
+        return relator = "You Lose! Scissors cut Paper"
     }
     else if (playerSelection === "ROCK" && computerChoice === "PAPER") {
         ++pcPoints;
-        return ("You Lose! Paper wrap Rock")
+        return relator = "You Lose! Paper wrap Rock"
     }
     else if (playerSelection === "PAPER" && computerChoice === "ROCK") {
         ++userPoints;
-        return ("You Win! Paper wrap Rock")
+        return relator = "You Win! Paper wrap Rock"
     }
     else if (playerSelection === "SCISSORS" && computerChoice === "ROCK") {
         ++pcPoints;
-        return ("You Lose! Rock beats Scissors")
+        return relator = "You Lose! Rock beats Scissors"
     }
     else if (playerSelection === "SCISSORS" && computerChoice === "PAPER") {
         ++userPoints;
-        return ("You Win! Scissors cut Paper")
+        return relator = "You Win! Scissors cut Paper"
     }
     else if (playerSelection === computerChoice) {
-        ++pcPoints;
-        ++userPoints;
-        return ("It's a Tie!!!")
+        return relator = "It's a Tie!!!"
     }
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerChoice));
+    if (!(pcPoints === 5 || userPoints === 5)) {
+        playRound(playerSelection);
         computerChoice = getComputerChoice();
     }
-    if (userPoints > pcPoints) {
-        return ("Congratulations!!! You are the WINNER!!")
-    } else {
-        return ("Sorry, you lose =( ")
+    if (userPoints === 5) {
+        setTimeout(() => finishGame() ,200)
+        return match = "Congratulations!!! You are the WINNER!!"
+    } else if (pcPoints === 5) {
+        setTimeout(() => finishGame() ,200)
+        return match = "Sorry, you lose =( "
     }
 }
 
-console.log(game());
+rock.addEventListener("click", () => { 
+    playRound("ROCK")
+    game()
+    round.textContent = relator
+    userScore.textContent = userPoints
+    pcScore.textContent = pcPoints
+})
+paper.addEventListener("click", () => { 
+    playRound("PAPER")
+    game()
+    round.textContent = relator
+    userScore.textContent = userPoints
+    pcScore.textContent = pcPoints
+});
+scissors.addEventListener("click", () => {
+    playRound("SCISSORS")
+    game()
+    round.textContent = relator
+    userScore.textContent = userPoints
+    pcScore.textContent = pcPoints
+});
